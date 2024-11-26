f4ba35111b2113620ab546cfb244eec5eda27fe9
Marina <64136269+marinasb28@users.noreply.github.com>
2024-11-26 21:21:22 +0100

    Add files via upload

---
+++ DA INTERVIEW/STREAMLIT/main.py
@@ -0,0 +1,159 @@
+import streamlit as st
+from pathlib import Path
+from streamlit_option_menu import option_menu
+import requests
+
+
+# 1_🏘️_Home
+# 2_👩🏾‍🎓_About
+# 3_📊_Tableau
+# 4_🔮_Prediction
+# 9_📧_Contact
+# GENERAL SETTINGS
+PAGE_TITLE = "Digital CV | Marina Suárez Blázquez"
+PAGE_ICON = ":briefcase:"
+DESCRIPTION = """
+NOTE: Junior data analyst, just starting her career.
+"""
+EMAIL = "msuarezblazquez@gmail.com"
+SOCIAL_MEDIA = {
+    "LinkedIn": "https://www.linkedin.com/in/marina-su%C3%A1rez-bl%C3%A1zquez-a4aa301bb/",
+    "GitHub": "https://github.com/marinasb28"
+}
+LOGOS = {
+    "LinkedIn": '<i class="fa-brands fa-linkedin"></i>',
+    "GitHub": '<i class="fa-brands fa-github"></i>'
+}
+
+# ---- PATH SETTINGS --- #
+
+# FILES
+current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
+css_file = current_dir / "styles" / "main.css"
+
+# PICTURES
+resume_file = current_dir / "assets" / "Resume_CV.pdf"
+profile_pic = current_dir / "assets" / "cap.jpg"
+
+# PAGES
+home = current_dir /"pages"/"1_🏘️_Home.py"
+about_me = current_dir /"pages"/"2_👩🏾‍🎓_About.py"
+tableau_pg = current_dir/"pages"/"3_📊_Tableau.py"
+prediction_pg = current_dir/"pages"/"4_🔮_Prediction.py"
+contact_me = current_dir /"pages"/"9_📧_Contact.py"
+
+# DATA
+hr_records = current_dir.parent/"TABLEAU"/"HR_data.csv"
+pred_data = current_dir/"data"/"survey_results_public.csv"
+pkl_s = current_dir/"data"/"salary_pred_model.pkl"
+# FUNCTIONS
+
+# Load animations
+def load_animation(url):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+
+# HOME FILE
+def run_home():
+    with open(home) as f:
+        exec(f.read(), {'__name__': '__main__'})
+
+
+# ABOUT ME FILE
+
+def run_about_me():
+    with open(about_me) as f:
+        exec(f.read(), {'__name__': '__main__'})
+
+# CONTACT FILE
+def run_contact_me():
+    with open(contact_me) as f:
+        exec(f.read(), {'__name__': '__main__'})
+
+# TABLEAU
+def run_tableau():
+    with open(tableau_pg) as f:
+        exec(f.read(), {'__name__': '__main__'})
+
+# PREDICTION
+def run_prediction():
+    with open(prediction_pg) as f:
+        exec(f.read(), {'__name__': '__main__'})
+
+
+# MAIN FUNCTION
+def main():
+    st.set_page_config(
+        page_title="Digital CV | MSB",
+        page_icon=PAGE_ICON
+    )
+
+    if 'page' not in st.session_state:
+        st.session_state.page = 'Home'  # Default page is Home
+
+    # Define the options
+    options = ["Home", "About me", "Tableau", "Prediction", "Contact"]
+    icons = ["house", "person-lines-fill", "bar-chart", "cookie", "envelope-at"]
+
+    # Display the option menu
+    selected = option_menu(
+        menu_title=None,
+        options=options,
+        icons=icons,
+        menu_icon="cast",
+        default_index=options.index(st.session_state.page),  # Sets default to session state's page
+        orientation="horizontal",
+        styles={
+            "container": {
+                "display": "flex",
+                "justify-content": "center",
+                "align-items": "center",
+                "width": "100%",
+                "padding": "0",
+            },
+            "icon": {
+                "color": "black",
+                "font-size": "20px",  # Adjust font size
+                "display": "inline-block",
+            },
+            "nav-link": {
+                "font-size": "20px",  # Adjust font size for the text
+                "text-align": "center",
+                "margin": "0 5px",  # Adjust margin for spacing
+                "--hover-color": "orange",
+                "padding": "10px",  # Uniform padding for links
+                "display": "flex",
+                "align-items": "center",
+                "white-space": "nowrap",  # Prevent text wrapping
+            },
+            "nav-link-selected": {
+                "background-color": "orange",
+                "color": "white",
+                "margin": "0 5px",  # Same margin as other links
+            }
+        }
+    )
+    
+    # Update session state based on selection
+    if selected:
+        st.session_state.page = selected
+
+    # Navigate to the correct page based on the selection
+    if st.session_state.page == "About me":
+        run_about_me()
+    elif st.session_state.page == "Contact":
+        run_contact_me()
+    elif st.session_state.page == "Home":
+        run_home()
+    elif st.session_state.page == "Tableau":
+        run_tableau()
+    elif st.session_state.page == "Prediction":
+        run_prediction()
+
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ DA INTERVIEW/STREAMLIT/pages/4_🔮_Prediction.py
@@ -0,0 +1,191 @@
+import streamlit as st
+import pandas as pd
+import pickle
+import matplotlib.pyplot as plt
+import numpy as np
+import main as mp
+
+if not st.session_state.page == "Prediction":
+    st.set_page_config(
+    page_title="Princing Prediction",
+    page_icon="🔮"
+    )
+
+
+# SIDEBAR menu
+st.sidebar.title("Pages")
+st.sidebar.page_link(mp.home,label="Home",icon="🏘️")
+st.sidebar.page_link(mp.about_me,label="About me",icon="👩🏾‍🎓")
+st.sidebar.page_link(mp.tableau_pg,label="Tableau",icon="📊")
+st.sidebar.page_link(mp.prediction_pg,label="Prediction",icon="🔮")
+st.sidebar.page_link(mp.contact_me,label="Contact me",icon="📧")
+
+# -- FUNCTIONS ------- #
+
+
+# Simplifica la impresión de los números
+def format_number(num):
+    if num >= 1_000_000:
+        return f"{num / 1_000_000:.1f}M"
+    elif num >= 1_000:
+        return f"{num / 1_000:.1f}K"
+    else:
+        return str(num)
+
+# Nos ayuda a deshacernos de aquellos países con pocos datos
+def cut_cat(category_data, threshold):
+    category_mapping = {}
+    for idx in range(len(category_data)):
+        if category_data.values[idx] >= threshold:
+            category_mapping[category_data.index[idx]] = category_data.index[idx]
+        else:  # if insufficient data, categorize as "Other"
+            category_mapping[category_data.index[idx]] = 'Other'
+    return category_mapping
+
+# Simplifica las strings
+def short_ed(x):
+    if 'Bachelor’s degree' in x:
+        return 'Bachelor’s degree'
+    if 'Master’s degree' in x:
+        return 'Master’s degree'
+    if 'Professional degree' in x or 'Other doctoral' in x:
+        return 'Post grad'
+    return 'Less than a Bachelors'
+
+#@st.cache_data
+def load_csv():
+
+    df = pd.read_csv(mp.pred_data)
+
+    keep = ["Country", "EdLevel", "YearsCodePro", "Employment", "ConvertedCompYearly"]
+    df = df[keep]
+
+    df = df = df.dropna(subset=['ConvertedCompYearly'])
+    df = df.dropna()
+    df = df[df["Employment"].str.contains("full-time", case=False, na=False)]
+    df = df.drop("Employment", axis=1)
+
+
+    country_map = cut_cat(df['Country'].value_counts(), 400)
+    df["Country"] = df["Country"].map(country_map)
+    df = df[df["ConvertedCompYearly"] <= 250000]
+    df = df[df["ConvertedCompYearly"] >= 10000]
+    df = df[df["Country"] != "Other"]
+
+    df['YearsCodePro'] = df['YearsCodePro'].apply(lambda x: 0.5 if x == 'Less than 1 year' else float(x))
+    df["EdLevel"] = df["EdLevel"].apply(short_ed)
+    df = df.rename({"ConvertedCompYearly": "Salary"}, axis=1)
+    
+    return df
+
+
+# Load the trained model
+def load_model():
+    with open(mp.pkl_s, 'rb') as file:
+        data = pickle.load(file)
+    return data
+
+
+# -- MAIN FUNCTIONS -- #
+def show_predict():
+    st.title("Software Developer Salary Prediction 💲")
+    st.warning("We need some information to predict the salary.")
+    data = load_model()
+
+    regressor = data["model"]
+    le_country = data["le_country"]
+    le_ed = data["le_education"]
+
+    # get country selector
+    country_list = (
+        "United States of America",
+        "United Kingdom of Great Britain and Northern Ireland",
+        "France",
+        "Brazil",
+        "Germany",
+        "Ukraine",
+        "Canada",
+        "Italy",
+        "India",
+        "Spain",
+        "Netherlands",
+        "Sweden",
+        "Poland",
+        "Australia"
+    )
+
+    # get education selector
+    ed_list = (
+        "Less than a Bachelors",
+        "Post grad",
+        "Master’s degree",
+        "Bachelor’s degree"
+    )
+
+
+    # FEATURES
+    country_selection = st.selectbox("**Country**", country_list)
+    ed_selection = st.selectbox("**Education level**", ed_list)
+    exp_years = st.slider("**Years of experience:**",0,50,3)
+
+    # Start prediction
+    on = st.toggle("Turn on to predict the salary 💸")
+
+
+    if on:
+        X = np.array([[country_selection, ed_selection, exp_years ]])
+        X[:, 0] = le_country.transform(X[:,0])
+        X[:, 1] = le_ed.transform(X[:,1])
+        X = X.astype(float)
+
+        salary = regressor.predict(X)
+        st.success(f"Predicted salary: ${format_number(salary[0])}")
+
+def show_explore():
+    st.title("Exploring Software Developer Salary 💲")
+
+    df = load_csv()
+    data = df["Country"].value_counts()
+
+    fig1, ax1 = plt.subplots()
+    ax1.pie(data, labels=data.index, autopct="%1.1f%%", shadow=True, startangle=90)
+    ax1.axis("equal")  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+    st.write("""#### Number of Data from different countries""")
+
+    st.pyplot(fig1)
+    
+    st.write(
+        """
+    #### Mean Salary Based On Country
+    """
+    )
+
+    data = df.groupby(["Country"])["Salary"].mean().sort_values(ascending=True)
+    st.bar_chart(data)
+
+    st.write(
+        """
+    #### Mean Salary Based On Experience
+    """
+    )
+
+    data = df.groupby(["YearsCodePro"])["Salary"].mean().sort_values(ascending=True)
+    st.line_chart(data)
+
+
+# -- MAIN CODE -- #
+st.sidebar.write("---")
+st.sidebar.title("Prediction 🔮")
+page = st.sidebar.selectbox("Explore or predict?",("Explore", "Predict"))
+
+if page == "Predict":
+    show_predict() 
+
+
+elif page == "Explore":
+    show_explore()
+
+else:
+    st.title("Old Prediction Project 🔮")
+    st.write("Hola")
