1bc1cbc466ace427d6e52332de29ed8c7170e0ed
Marina <msuarezblazquez@gmail.com>
2024-12-06 22:51:25 +0100

    .

--- INTERVIEW/INTERRADISH/CSharp_1/Program.cs
+++ INTERVIEW/INTERRADISH/CSharp_1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic; //lists, collections...
+using System.Linq; // work with data
+using System.Text; // text & coding
+using System.Threading.Tasks; // threading application
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
