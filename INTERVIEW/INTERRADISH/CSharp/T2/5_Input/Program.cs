171548bab37ec4411c3cb50ae576ce928bbefe3d
Marina <msuarezblazquez@gmail.com>
2024-12-07 22:51:06 +0100

    ready para terminar

---
+++ INTERVIEW/INTERRADISH/CSharp/T2/5_Input/Program.cs
@@ -0,0 +1,17 @@
+﻿
+namespace T2
+{
+    public static class Input
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("What's your name? ");
+            string name = Console.ReadLine();
+
+            Console.Write($"Hello, {name}! How old are you? ");
+            int age = int.Parse(Console.ReadLine());
+            //int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"You are {age} years old.");
+        }
+    }
+}
---
+++ INTERVIEW/INTERRADISH/CSharp/T2/6_Calculator/Program.cs
@@ -0,0 +1,16 @@
+﻿
+namespace T2
+{
+    public static class Calculator
+    {
+        public static void Main(string[] args)
+        {
+            int num1, num2;
+            Console.Write("Enter a number: ");
+            num1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter a second number: ");
+            num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Sum: {num1 + num2}");
+        }
+    }
+}
