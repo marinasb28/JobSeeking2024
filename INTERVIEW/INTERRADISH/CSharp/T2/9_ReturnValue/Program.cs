dd320cdb990424bce78020978cd4fed7d6579ed1
Marina <msuarezblazquez@gmail.com>
2024-12-07 23:39:10 +0100

    if cases

---
+++ INTERVIEW/INTERRADISH/CSharp/T2/9_ReturnValue/Program.cs
@@ -0,0 +1,18 @@
+﻿
+namespace T2
+{
+    public static class ReturnValue
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Insert a number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{number}^3 = {CubeNum(number)}");
+        }
+
+        static int CubeNum(int num)
+        {
+            return (int)Math.Pow(num,3);
+        }
+    }
+}
