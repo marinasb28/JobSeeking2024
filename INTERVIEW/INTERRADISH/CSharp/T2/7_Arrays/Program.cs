1a0d705fc3a1d23c0c52d0e572ca068a776df27f
Marina <msuarezblazquez@gmail.com>
2024-12-07 23:13:04 +0100

    methods

---
+++ INTERVIEW/INTERRADISH/CSharp/T2/7_Arrays/Program.cs
@@ -0,0 +1,17 @@
+﻿
+namespace T2
+{
+    public static class Arrays
+    {
+        public static void Main(string[] args)
+        {
+            //Array of intergers
+            int[] luckyNumbers = {7,13,21};
+
+            // Change value
+            Console.WriteLine($"Before: {luckyNumbers[1]}");
+            luckyNumbers[1] = 15;
+            Console.WriteLine($"After: {luckyNumbers[1]}");
+        }
+    }
+}
---
+++ INTERVIEW/INTERRADISH/CSharp/T2/8_Methods/Program.cs
@@ -0,0 +1,23 @@
+﻿
+namespace T2
+{
+    public static class Methods
+    {
+        public static void Main(string[] args)
+        {
+            string name;
+            int age;
+            Console.Write("What's your name? ");
+            name = Console.ReadLine();
+            Console.Write("And your age? ");
+            age = int.Parse(Console.ReadLine());
+            SayHi(name,age);
+        }
+
+        static void SayHi(string n,int a)
+        {
+            Console.WriteLine($"Hello {n}! Wow, you're {a} old.");
+
+        }
+    }
+}
