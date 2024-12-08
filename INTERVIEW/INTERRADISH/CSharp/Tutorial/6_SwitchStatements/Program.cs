39eeb0ba2b81ef15357d2024fe33513cff0c79b7
Marina <msuarezblazquez@gmail.com>
2024-12-08 21:59:41 +0100

    1st hour

--- INTERVIEW/INTERRADISH/CSharp/Tutorial/6_SwitchStatements/Program.cs
+++ INTERVIEW/INTERRADISH/CSharp/Tutorial/6_SwitchStatements/Program.cs
@@ -4,6 +4,27 @@ namespace Tutorial
     public class SwitchStatements
     {
         static void Main(string[] args)
-        {}
+        {
+            Console.Write("Enter a number (1-3): ");
+            int numLight = int.Parse(Console.ReadLine());
+
+            string lightName;
+            switch(numLight)
+            {
+                case 1:
+                    lightName = "Red";
+                    break;
+                case 2:
+                    lightName = "Yellow";
+                    break;
+                case 3:
+                    lightName = "Green";
+                    break;
+                default:
+                    lightName = "N/A";
+                    break;
+            }
+            Console.WriteLine($"{lightName} is {numLight}");
+        }
     }
 }
