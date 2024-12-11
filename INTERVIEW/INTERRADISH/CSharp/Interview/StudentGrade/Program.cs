a424014f442c7274b853fabbb1e4f60cf7197f8d
Marina <msuarezblazquez@gmail.com>
2024-12-11 22:23:02 +0100

    student grade

---
+++ INTERVIEW/INTERRADISH/CSharp/Interview/StudentGrade/Program.cs
@@ -0,0 +1,46 @@
+﻿
+namespace StudentGrade
+{
+    public class StudentGrade
+    {
+        static (double, string) avgGrade(List<double> grades)
+        {
+            double sum = 0;
+            foreach(var grade in grades)
+            {
+                sum += grade;
+            }
+           double avg = sum / grades.Count;
+
+            string letterGrade = "";
+            if (avg >= 90)
+            {
+                letterGrade = "A";
+            }
+            else if (avg >= 80)
+            {
+                letterGrade = "B";
+            }
+            else if (avg >= 70)
+            {
+                letterGrade = "C";
+            }
+            else if (avg >= 60)
+            {
+                letterGrade = "D";
+            }
+            else
+            {
+                letterGrade = "F";
+            }
+            return (avg, letterGrade);
+        }
+        static void Main(string[] args)
+        {
+            List<double> grades = new List<double>() { 85,90,78,92};
+            
+            var result = avgGrade(grades);
+            Console.WriteLine($"Average grade: {result.Item1}, Letter grade: {result.Item2}");
+        }
+    }
+}
