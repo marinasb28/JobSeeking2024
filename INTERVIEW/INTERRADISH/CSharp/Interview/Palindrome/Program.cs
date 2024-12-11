420a7e97775e31cf27956a59f27810b9f9046ec6
Marina <msuarezblazquez@gmail.com>
2024-12-11 22:27:14 +0100

    done

--- INTERVIEW/INTERRADISH/CSharp/Interview/Palindrome/Program.cs
+++ INTERVIEW/INTERRADISH/CSharp/Interview/Palindrome/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a string:");
+        string input = Console.ReadLine();
+
+        bool isPalindrome = IsPalindrome(input);
+        Console.WriteLine($"Is palindrome: {isPalindrome}");
+    }
+
+    static bool IsPalindrome(string str)
+    {
+        // Remove non-alphanumeric characters and convert to lowercase
+        string cleanedStr = string.Concat(str.Where(c => Char.IsLetterOrDigit(c))).ToLower();
+
+        // Compare characters from start to end
+        int left = 0;
+        int right = cleanedStr.Length - 1;
+
+        while (left < right)
+        {
+            if (cleanedStr[left] != cleanedStr[right])
+            {
+                return false; // Not a palindrome if characters don't match
+            }
+            left++;
+            right--;
+        }
+
+        return true; // All characters matched, it's a palindrome
+    }
+}
